504e18e6293768299fa60d3774a2daea265da27f
waldos09 <legos567@gmail.com>
2020-03-22 19:23:20 -0600

    proyecto con php para heroku

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Pong</title>
+    <style>
+        canvas {
+            border: solid;
+        }
+    </style>
+</head>
+
+<body>
+
+    <h1>Ping Pong </h1>
+    <h1><a href="pong_v2.html">Ping Pong v2</a></h1>
+    <canvas id="canvas"></canvas>
+    <script src="js/main.js"></script>
+
+</body>
+
+</html>
---
+++ pong_v2.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Pong Example</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
+    <style>
+        canvas {
+            border: solid
+        }
+    </style>
+
+</head>
+
+<body>
+    <div class="container">
+        <div class="row text-center">
+            <h1>Ping Pong v2</h1>
+        </div>
+        <div class="row text-center">
+            <h2>
+                <span class="label label-primary">Team A</span>
+                <label id="teamA">0</label> -
+                <label id="teamB">0</label>
+                <span class="label label-danger">Team B</span>
+            </h2>
+        </div>
+        <div class="row text-center">
+            <canvas id="canvas"></canvas>
+        </div>
+    </div>
+    <script src="js/main_v2.js"></script>
+</body>
+
+</html>
